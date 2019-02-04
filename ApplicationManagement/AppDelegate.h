ada55e537f67cec41bc4331fce1b2a5d112260bd
Rui Cao <rayray199085@gmail.com>
2019-02-05 08:32:16 +1100

    Initial Commit

---
+++ ApplicationManagement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApplicationManagement
+//
+//  Created by Stephen Cao on 5/2/19.
+//  Copyright © 2019 Stephen Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ApplicationManagement/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApplicationManagement
+//
+//  Created by Stephen Cao on 5/2/19.
+//  Copyright © 2019 Stephen Cao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
